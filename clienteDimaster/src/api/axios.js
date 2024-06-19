f13f27eac007e01f6e66e8c676a30f91af36068d
Juan <workpulgarin@gmail.com>
2024-06-19 10:25:39 -0500

    render

--- clienteDimaster/src/api/axios.js
+++ clienteDimaster/src/api/axios.js
@@ -1,19 +1,19 @@
-// import axios from "axios"
-
-// const instance = axios.create({
-//     baseURL:"https://api-servidor-d8f1.onrender.com/api/v1",
-//     withCredentials:true
-
-// })
-
-// export default instance
-
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL:"http://localhost:3000/api/v1",
+    baseURL:"https://api-servidor-d8f1.onrender.com/api/v1",
     withCredentials:true
 
 })
 
 export default instance
+
+// import axios from "axios"
+
+// const instance = axios.create({
+//     baseURL:"http://localhost:3000/api/v1",
+//     withCredentials:true
+
+// })
+
+// export default instance
